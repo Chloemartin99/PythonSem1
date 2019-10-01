36b80ef75486793603fdb22adafe50ac3dcc315d
chloemartin <cm.ieu2018@student.ie.edu>
2019-10-01 21:44:33 +0200

    homework - checking code with a list that stores

--- Sessions/Sess7_8/homework.py
+++ Sessions/Sess7_8/homework.py
@@ -1,20 +1,22 @@
 s = "azcbobobegghakl"
 current = ""
 longest = ""
-length = len(current)
+store=[]
 
 for i in s:
     if current=="":
         current = i
 
-    if i<=current[-1]:
+    if i< current[-1]:
         if len(current) > len(longest):
+            store.append(current)
             longest = current
 
-        else:
-            current = ""
+        current = i
 
     else:
         current = current+i
 
+
 print(longest)
+print(store)
