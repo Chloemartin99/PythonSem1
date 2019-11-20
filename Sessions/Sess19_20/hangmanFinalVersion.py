4a545b734aabc370a5bd82b24c6da4df04073b62
chloemartin <cm.ieu2018@student.ie.edu>
2019-11-20 01:12:33 +0100

    Hangman Homework 2

--- Sessions/Sess19_20/hangmanFinalVersion.py
+++ Sessions/Sess19_20/hangmanFinalVersion.py
@@ -1,119 +1,150 @@
 import random
 from tkinter import *
 from PIL import ImageTk, Image
+import sqlite3
 
-def checkLetter():
-    global done
-    global letter_choice
-    if var_choose.get() not in done:
-        done.append(var_choose.get())
+class Hangman(object):
+    def __init__(self, window):
+        self.window = window
+        self.window.title("Hangman by Chloe Martin")
+        self.font = ("Helvetica", 40)
 
-    txt = ""
-    for i in range(len(done)):
-        txt+=done[i]+'\u0336'+"  "
+        db = sqlite3.connect("hangman_records.db")
+        cur = db.cursor()
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS hangman(id INTEGER PRIMARY KEY, word VARCHAR, trials INTEGER, result varchar)")
+        db.commit()
+        db.close()
 
-        lb = Label(master=window, text=txt, font=font)
-        lb.grid(row=9, column=1)
+        self.lb = Label(master=self.window, text="A Hangman's Story", font=("Helvetica", 60), fg="blue")
+        self.lb.grid(row=1, column=2, padx=10, pady=5)
 
-    if var_choose.get() in word:
-        p = word.index(var_choose.get())
-        positions[p] = var_choose.get()
+        self.bt = Button(master=self.window, text="Exit", fg="red", font=self.font, command=sys.exit)
+        self.bt.grid(row=0, column=6)
 
-        temp = word
-        temp[p]="-"
-        if var_choose.get() in temp:
-            p2 = temp.index(var_choose.get())
-            positions[p2] = var_choose.get()
+        self.lb = Label(master=self.window, text="Word: ", font=self.font)
+        self.lb.grid(row=3, column=1, sticky="e")
 
-        lb_pos = Label(master=window, text=positions, font=font, fg="blue")
-        lb_pos.grid(row=3, column=2)
-    else:
-        updateImage()
-    checkHealth()
-def updateImage():
-    global image_counter
-    image_counter+=1
+        self.bt = Button(master=self.window, text="Save Game", fg="red", font=self.font, command=self.saveGame)
+        self.bt.grid(row=1, column=6)
 
-    if image_counter<=13:
-        im2 = Image.open("S" + str(image_counter) + ".jpeg")
+        ran = random.randint(0, 1000)
+        file = open("1-1000.txt", "r")
+        temp = file.read().splitlines()
+        pick = temp[ran]
+
+        self.letters = list(pick)
+        self.word = ""
+        for i in range(len(self.letters)):
+            self.word+=self.letters[i]
+
+        self.image_counter = 0
+        self.done = []
+
+        self.positions = []
+
+        for i in range(len(self.word)):
+            self.positions.append("_")
+
+        self.lb_pos = Label(master=self.window, text=self.positions, font=self.font, fg="blue")
+        self.lb_pos.grid(row=3, column=2)
+
+        self.lb = Label(master=self.window, text="Choose Letter:", font=self.font)
+        self.lb.grid(row=8, column=1, sticky="e")
+
+        let = "abcdefghijklmnopqrstuvwxyz"
+        self.letter_choice = []
+        for i in let:
+            self.letter_choice.append(i)
+
+        self.var_choose = StringVar(window)
+        self.var_choose.set(self.letter_choice[0])
+
+        self.op = OptionMenu(self.window, self.var_choose, *self.letter_choice)
+        self.op.grid(row=8, column=2)
+        self.op.config(font=self.font)
+
+        self.trials = 0
+
+        self.bt = Button(master=self.window, text="GO!", font=self.font, command=self.checkLetter)
+        self.bt.grid(row=9, column=2)
+
+        self.canvas = Canvas(window, width=420, height=520)
+        im = Image.open("S0.jpeg")
         size = 600, 520
-        im2.thumbnail(size)
-        canvas.image= ImageTk.PhotoImage(im2)
-        canvas.create_image(220, 6, image=canvas.image, anchor="n")
-    else:
-        lb = Label(master=window, text="The word was " + word, font=font, fg="red")
-        lb.grid(row=12, column=1)
+        im.thumbnail(size)
 
-def checkHealth(): #doesn't work
-    pass
-    #if image_counter==13:
-     #   lb = Label(master=window, text="The word was "+word, font=font, fg="red")
-      #  lb.grid(row=12, column=1)
-       # return False
-    #else:
-     #   for i in positions:
-      #      if i == "_":
-       #         return False
-    #lb = Label(master=window, text="VICTORY!", font=font, fg="green")
-    #lb.grid(row=12, column=1)
+        self.canvas.image = ImageTk.PhotoImage(im)
+        self.canvas.create_image(220, 6, image=self.canvas.image, anchor="n")
+        self.canvas.grid(row=9, column=6)
+
+    def checkLetter(self):
+        self.checkVictory()
+        if self.var_choose.get() not in self.done:
+            if self.var_choose.get() in self.letters:
+                p = self.letters.index(self.var_choose.get())
+                self.positions[p] = self.var_choose.get()
+
+                temp = self.letters
+                temp[p] = "-"
+                if self.var_choose.get() in temp:
+                    p2 = temp.index(self.var_choose.get())
+                    self.positions[p2] = self.var_choose.get()
+
+                self.lb_pos = Label(master=self.window, text=self.positions, font=self.font, fg="blue")
+                self.lb_pos.grid(row=3, column=2)
+
+            else:
+                self.done.append(self.var_choose.get())
+                txt = ""
+                for i in range(len(self.done)):
+                    txt += self.done[i] + '\u0336' + "  "
+
+                    self.lb = Label(master=self.window, text=txt, font=self.font)
+                    self.lb.grid(row=9, column=1)
+                self.updateImage()
+            self.trials += 1
+
+    def checkVictory(self):
+        correct_letters = ""
+        for i in self.positions:
+            correct_letters += i
+
+        print(correct_letters)
+        if correct_letters == self.word:
+            self.lb = Label(master=self.window, text="***VICTORY!***", font=("Helvetica", 50), fg="green")
+            self.lb.grid(row=7, column=1)
+            return True
+        return False
+
+    def updateImage(self):
+        self.image_counter += 1
+
+        if self.image_counter <= 13:
+            im2 = Image.open("S" + str(self.image_counter) + ".jpeg")
+            size = 600, 520
+            im2.thumbnail(size)
+            self.canvas.image = ImageTk.PhotoImage(im2)
+            self.canvas.create_image(220, 6, image=self.canvas.image, anchor="n")
+
+        elif self.checkVictory() is False:
+            self.lb = Label(master=self.window, text="The word was " + self.word, font=self.font, fg="red")
+            self.lb.grid(row=12, column=1)
+
+    def saveGame(self):
+        result = ""
+        if self.checkVictory() is True:
+            result = "Won"
+        else:
+            result = "Lost"
+
+        db = sqlite3.connect("hangman_records.db")
+        cur = db.cursor()
+        cur.execute("INSERT INTO hangman VALUES(NULL, ?, ?, ?)", (self.word, self.trials, result))
+        db.commit()
+        db.close()
 
 window = Tk()
-window.title("Hangman by Chloe Martin")
-
-font = ("Helvetica", 40)
-
-lb = Label(master=window, text="A Hangman's Story", font=("Helvetica", 60), fg="blue")
-lb.grid(row=1, column=2, padx=10, pady=5)
-
-bt = Button(master=window, text="Exit", fg="red", font=font, command=sys.exit)
-bt.grid(row=0, column=6)
-
-lb = Label(master=window, text="Word: ", font=font)
-lb.grid(row=3, column=1, sticky="e")
-
-random = random.randint(0, 1000)
-file = open("1-1000.txt", "r")
-temp = file.read().splitlines()
-pick = temp[random]
-
-word = list(pick)
-image_counter = 0
-done=[]
-
-positions = []
-
-for i in range(len(word)):
-    positions.append("_")
-
-lb_pos = Label(master=window, text=positions, font=font, fg="blue")
-lb_pos.grid(row=3, column=2)
-
-lb = Label(master=window, text="Choose Letter:", font=font)
-lb.grid(row=8, column=1, sticky="e")
-
-let="abcdefghijklmnopqrstuvwxyz"
-letter_choice = []
-for i in let:
-    letter_choice.append(i)
-
-var_choose = StringVar(window)
-var_choose.set(letter_choice[0])
-
-op = OptionMenu(window, var_choose, *letter_choice)
-op.grid(row= 8, column=2)
-op.config(font=font)
-
-bt = Button(master=window, text="GO!", font=font, command=checkLetter)
-bt.grid(row=9, column=2)
-
-canvas = Canvas(window, width=420, height=520)
-im = Image.open("S0.jpeg")
-size = 600,520
-im.thumbnail(size)
-
-canvas.image = ImageTk.PhotoImage(im)
-canvas.create_image(220, 6, image=canvas.image, anchor="n")
-canvas.grid(row=9, column=6)
-
+h = Hangman(window)
+print(h.word)
 window.mainloop()
-window.close()
